aff10b44dc75b50633431bd02bc33d8e32fb47e1
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-04-16 19:10:09 -0700

    wip: first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Calculator App
+## This is a simple calculator web app that allows you to perform mathematical functions such as addition, substraction, multiplcation, and division
+### Languages used: 
+* JavaScript
+* HTML
+* CSS
+
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert("JS is working")
+const $button = $("button");
+const $equal = $("#equal");
+const $input = $("#myText");
+
+$button.click(function(){
+    let aVal = $(this).val();
+    $input.val($input.val() + aVal);
+});
+
+let aResult = (eval($input.val()))
+
+$equal.click(function(){
+    let aResult= (eval($input.val()));
+    console.log(aResult);
+    $input.val($input.val() + aResult);
+});
+
+
+
+
